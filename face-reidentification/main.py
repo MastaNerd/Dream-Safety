686a48b4666fc8a74706dcadfc96f653f268c4f9
MastaNerd <vibhush.s.sivakumar@gmail.com>
2025-11-18 22:45:40 -0500

    Added Text to Speech

--- face-reidentification/main.py
+++ face-reidentification/main.py
@@ -14,7 +14,7 @@ sio_app = socketio.ASGIApp(sio)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
